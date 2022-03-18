15a72a1a88be9e70f9591b2a4764f66365680fe1
Tobias <tobiasurban@hotmail.com>
2022-03-18 10:45:44 +0100

    added item class

---
+++ src/Item.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Item {
+
+void itemm () {
+    String[] items = {"item 1", "item 2", "item 4", "osv..."};
+    ArrayList<String> item = new ArrayList<>(List.of(items));
+
+    item.add("item 4");
+    item.add("item 5");
+
+}
+
+
+}
--- src/Player.java
+++ src/Player.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Player {
   private Room currentRoom;
 
+
   private String playerName;
   private String newLoc;
   String cantGo = "you can't go that way";
@@ -60,46 +61,4 @@ public class Player {
     }
   }
 
-  /*void north() {
-    if (game.getCommand() !=) {
-      System.out.println(newLoc);
-      currentRoom = game.getCurrentRoom().getSouth();
-    } else {
-      System.out.println(cantGo);
-    }
-  }
-}
-/*
- case "go north", "north", "go n", "n" -> {
-     if (currentRoom.getNorth() != null) {
-     System.out.println(newLoc);
-     currentRoom = currentRoom.getNorth();
-     } else {
-
-     System.out.println(cantGo);
-     }
-     }
-     case "go south", "south", "go s", "s" -> {
-     if (currentRoom.getSouth() != null) {
-     System.out.println(newLoc);
-     currentRoom = currentRoom.getSouth();
-     } else {
-     System.out.println(cantGo);
-     }
-     }
-     case "go west", "west", "go w", "w" -> {
-     if (currentRoom.getWest() != null) {
-     System.out.println(newLoc);
-     currentRoom = currentRoom.getWest();
-     } else {
-     System.out.println(cantGo);
-     }
-     }
-     case "go east", "east", "go e", "e" -> {
-     if (currentRoom.getEast() != null) {
-     System.out.println(newLoc);
-     currentRoom = currentRoom.getEast();
-     } else {
-     System.out.println(cantGo);
-     }*/
 }
