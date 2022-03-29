7d662c36fdc4edd0d553a8ecb4b445d897ab6fef
Tobias <tobiasurban@hotmail.com>
2022-03-29 14:56:12 +0200

    added class Enemy

--- src/Enemy.java
+++ src/Enemy.java
@@ -1,6 +1,24 @@
 public class Enemy {
 
+  private String name;
+  private String description;
+  private int healthPointsEnemy;
+
   Enemy(String name, String description, int healthPointsEnemy) {
-    
+    this.name = name;
+    this.description = description;
+    this.healthPointsEnemy = healthPointsEnemy;
+  }
+  public String getName(){
+    return name;
+  }
+  public String getDescription(){
+    return description;
+  }
+  public int getHealthPointsEnemy(){
+    return healthPointsEnemy;
+  }
+  public void setHealthPointsEnemy(int healthPointsEnemy){
+    this.healthPointsEnemy = healthPointsEnemy;
   }
 }
